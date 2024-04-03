f69ea0c1050c25e325a6e048118cc8264f5fe32b
Clara Antunes <claraantunes.work@gmail.com>
2024-04-03 14:45:33 +0100

    Adicionei Getters e o Método Igual() no Piece.cs

--- GaloDaVelha/Piece.cs
+++ GaloDaVelha/Piece.cs
@@ -25,3 +25,54 @@ public enum Furo
     Sem
 }
 
+public class Piece
+{
+    private Tamanho tamanho;
+    private Cor cor;
+    private Forma forma;
+    private Furo furo;
+
+    public Piece(Tamanho size, Cor color, Forma form, Furo hole)
+    {
+        tamanho = size;
+        cor = color;
+        forma = form;
+        furo = hole;
+    }
+
+    public Tamanho GetTamanho()
+    {
+        return tamanho;
+    }
+
+    public Cor GetCor()
+    {
+        return cor;
+    }
+
+    public Forma GetForma()
+    {
+        return forma;
+    }
+
+    public Furo GetFuro()
+    {
+        return furo;
+    }
+
+    /// <summary>
+    /// Checa se as peças tem caracteristicas em comum, se tiver pelo menos uma
+    /// igual, retorna True
+    /// </summary>
+    /// <param name="outra"></param>
+    /// <returns></returns>
+    public bool Igual(Piece outra)
+    {
+        return tamanho == outra.GetTamanho() ||
+               cor == outra.GetCor() ||
+               forma == outra.GetForma() ||
+               furo == outra.GetFuro();
+    }
+
+
+}

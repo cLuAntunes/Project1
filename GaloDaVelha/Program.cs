1bb859ef751fcdc933c11cd9ddfc5e803c530a21
Clara Antunes <claraantunes.work@gmail.com>
2024-04-03 12:41:39 +0100

    Adicionei um pedido de nome ao jogador Program.cs

--- GaloDaVelha/Program.cs
+++ GaloDaVelha/Program.cs
@@ -6,7 +6,15 @@ namespace GaloDaVelha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem-vindo ao GaloDaVelha!");
+
+            Console.Write("Nome do Jogador 1: ");
+            string nomePlayer1 = Console.ReadLine();
+            Player player1 = new Player(nomePlayer1);
+
+            Console.Write("Nome do Jogador 2: ");
+            string nomePlayer2 = Console.ReadLine();
+            Player player2 = new Player(nomePlayer2);
         }
     }
 }

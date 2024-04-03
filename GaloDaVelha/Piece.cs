9adbf911ba7d0651671165dd759c1e7add5cbce4
Clara Antunes <claraantunes.work@gmail.com>
2024-04-03 13:45:06 +0100

    Adicionei as regras no Program.cs e enums com as caracteristicas das peças no Piece.cs

---
+++ GaloDaVelha/Piece.cs
@@ -0,0 +1,27 @@
+/// <summary>
+/// Enumerações com as caracteristicas das peças
+/// </summary>
+public enum Tamanho
+{
+    Grande,
+    Pequena 
+}
+
+public enum Cor
+{
+    Clara,
+    Escura
+}
+
+public enum Forma
+{
+    Redonda,
+    Quadrada
+}
+
+public enum Furo
+{
+    Com,
+    Sem
+}
+
--- GaloDaVelha/Program.cs
+++ GaloDaVelha/Program.cs
@@ -8,6 +8,12 @@ namespace GaloDaVelha
         {
             Console.WriteLine("Bem-vindo ao GaloDaVelha!");
 
+            Console.WriteLine("Instruções de Jogo:");
+            Console.WriteLine("1. Cada jogador escolhe uma peça para o adversário colocar no tabuleiro.");
+            Console.WriteLine("2. O objetivo é formar uma linha de quatro peças com pelo menos uma característica semelhante.");
+            Console.WriteLine("3. Uma linha pode ser vertical, horizontal ou diagonal.");
+            Console.WriteLine("4. As características são: tamanho(Grande ou Pequena), cor(Clara ou Escura), forma(Redonda ou Quadrada) e furo(Com ou Sem).");
+
             Console.Write("Nome do Jogador 1: ");
             string nomePlayer1 = Console.ReadLine();
             Player player1 = new Player(nomePlayer1);
